10f288122796a33f4eb70154bea4d9e36aa0336f
{ KILLHAPPY } <graybobo@live.cn>
2017-11-20 14:10:30 +0800

    UA Examine

---
+++ assets/ua.js
@@ -0,0 +1,2 @@
+/*! UA Examine */
+try{var uAgent=(window.navigator&&navigator.userAgent)||"";function isIE(v){if(v){var __UA=uAgent.toUpperCase(),v_str="MSIE "+v+".0";if(__UA.indexOf(v_str)!=-1){return true}else{return false}}else{return(navigator.appName==="Microsoft Internet Explorer")}}if(isIE(9)||isIE(8)||isIE(7)||isIE(6)){window.location.href="/tips.html"}}catch(e){};
